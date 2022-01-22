e474395242b0b7289f52fbe5bc9bbe20e8a44456
José Pablo Chaves <jpablochaves@gmail.com>
2022-01-22 14:35:05 -0600

    Ejercicio 1 "tarea"

---
+++ Ejercicio_Clase1.py
@@ -0,0 +1,18 @@
+  # -*- coding: utf-8 -*-
+"""
+Author: José Pablo Chaves
+Fecha: 22-01-2022
+Ejercicio de Clase 1 sobre Estructuras de datos
+"""
+"""
+#1. Crear una lista con 5 nombres (str) 
+#2. Crear una litsa con 5 países (str)
+#3. Lista con 5 edades (int)
+#4. Lista con 5 salarios (float)
+#5. Dictionary con llaves > name, country, age, salary 
+# El dictinary debe almacenar las variables previas
+#6. Importar el paquete pandas con su alias pd
+#7. Convertir el dict a un DataFrame
+"""    
+nombres = ['Caro','Pablo','Jose','Gaby','Valentina']
+print(nombres)
